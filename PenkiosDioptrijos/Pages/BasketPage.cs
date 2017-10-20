7ff973c3d40d35220e6b35118a7f64e609542195
laggorazh <vaidas.maciulis@gmail.com>
2017-10-20 13:06:42 +0300

    Added cancel offer method

--- PenkiosDioptrijos/Pages/BasketPage.cs
+++ PenkiosDioptrijos/Pages/BasketPage.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using System;
 
 namespace Test
 {
@@ -11,10 +12,24 @@ namespace Test
         }
 
         public By ProceedToCheckoutButtonSelector = By.CssSelector("input.a-button-input[name=proceedToCheckout]");
+        public By PopUpHeaderTitleSelector = By.CssSelector("h4.a-popover-header-content");
+        public By NoThanksButtonSelector = By.CssSelector("button#siNoCoverage-announce");
 
         public void ProceedToCheckout()
         {
             Driver.FindElement(ProceedToCheckoutButtonSelector).Click();
+        }
+
+        public void CancelOfferPopUp()
+        {
+            try
+            {
+                Driver.FindElement(NoThanksButtonSelector).Click();
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Offer not shown");
+            }
         }
     }
 }
