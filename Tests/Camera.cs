9e970dd058b384e965ccdaa4a986acedd6d6855f
Sarunas <sarunas.zukas@gmail.com>
2017-10-20 12:49:28 +0300

    Camera workflow

--- Tests/Camera.cs
+++ Tests/Camera.cs
@@ -8,8 +8,22 @@ namespace Tests
 {
     class Camera
     {
-        /* 
-         
+      
+        /*  Search field:
+         *  1) Playstation 4 camera
+         *  
+         *  Filter: 
+         *  
+         *  1) Ships to Lithuania
+         *  2) De:select - Availability: Include Out of Stock
+         *  
+         *  Sort:
+         *  1) By price - 'Relevance'
+         *  
+         *  Select item which have Amazon`s Choice
+         *  //span[@class='a-badge-text' and contains(text(),'Amazon')]/../../../../../../../..
+         * 
+         * !!!!Save to variables price and delivery date!!!!
          */
     }
 }
