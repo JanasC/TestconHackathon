d4d87e2f0c1dcc4f08dd8d546305c3b7dcbdbe61
laggorazh <vaidas.maciulis@gmail.com>
2017-10-20 12:44:33 +0300

    moved method to basket page

--- PenkiosDioptrijos/Pages/ProductPage.cs
+++ PenkiosDioptrijos/Pages/ProductPage.cs
@@ -12,17 +12,12 @@ namespace Test
 
         public By AddToCartButtonSelector = By.CssSelector("div.nav-right input");
         public By ContinueToCartButtonSelector = By.CssSelector("form#smartShelfFormContinue input#smartShelfAddToCartNative");
-        public By ProceedToCheckoutButtonSelector = By.CssSelector("input.a-button-input[name=proceedToCheckout]");
-        
+
         public void AddToCart()
         {
             Driver.FindElement(AddToCartButtonSelector).Click();
             Driver.FindElement(ContinueToCartButtonSelector).Click();
         }
 
-        public void ProceedToCheckout()
-        {
-            Driver.FindElement(ProceedToCheckoutButtonSelector).Click();
-        }
     }
 }
