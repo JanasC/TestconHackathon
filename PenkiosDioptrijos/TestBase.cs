1e4e7233e2f906a5a99804707760702f6a5c363f
JanasC <janaschatkevicius@Janass-MacBook-Pro.local>
2017-10-20 15:36:44 +0300

    App config + cross browser testing

--- PenkiosDioptrijos/TestBase.cs
+++ PenkiosDioptrijos/TestBase.cs
@@ -1,18 +1,39 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using OpenQA.Selenium.Firefox;
+using System.Configuration;
 
 namespace PenkiosDioptrijos
 {
     public class TestBase
     {
-        
-        private readonly IWebDriver Driver = new ChromeDriver();
+        private IWebDriver Driver;
+
         public IWebDriver GetDriverInstance()
         {
+            Driver = GetNewDriverInstance();
             Driver.Manage().Window.Maximize();
             Driver.Navigate().GoToUrl("https://www.amazon.com/account");
             Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1);
+            return Driver;
+        }
+
+        public IWebDriver GetNewDriverInstance()
+        {
+            
+            string browser = ConfigurationManager.AppSettings["Browser"];
+            switch (browser)
+            {
+                case "Firefox":
+                    Driver = new FirefoxDriver();
+                    break;
+                case "Chrome":
+                    Driver = new ChromeDriver("/Users/janaschatkevicius/Desktop/");
+                    break;
+                default:
+                    break;
+            }
             return Driver;
         }
 
