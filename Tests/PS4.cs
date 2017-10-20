ee8aa8f500f6424795d6f03064a86ea587c5e55c
Sarunas <sarunas.zukas@gmail.com>
2017-10-20 12:08:15 +0300

    Test case playstation
    Test case playstation

---
+++ Tests/PS4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class PS4
+    {
+
+        /*  Search field:
+         *  1) Playstation 4 pro console 
+         *  
+         *  Filter: 
+         *  1) Video Games -> PlayStation 4 Consoles
+         *  2) Ships to Lithuania
+         *  3) Avg. Customer Review - **** and up.
+         *  4) De:select - Availability: Include Out of Stock
+         *  
+         *  Sort:
+         *  1) By price - 'low to high'
+         */
+
+    }
+}
