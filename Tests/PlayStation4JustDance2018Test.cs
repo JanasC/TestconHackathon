0ff7348012cc51281a4fbf7ac362bb0cd410a4a3
laggorazh <vaidas.maciulis@gmail.com>
2017-10-20 13:45:47 +0300

    merge

---
+++ Tests/PlayStation4JustDance2018Test.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Self)]
+    class PlayStation4JustDance2018Test
+    {
+
+        /* Search: 'Just Dance 2018 - PS4'
+         * 
+         * Select: Playstation 4 link
+                    driver.findElement(By.cssSelector('[data-attribute="Playstation 4"]'))
+
+        * Check platform (PS4)
+
+        * Select checkbox:  "Buy digital : $ 59 99" 
+        
+        * Add Release date and time to VARIABLE. 
+        * 
+        * Select: Pre-order Now
+         
+         
+         */
+    }
+}
