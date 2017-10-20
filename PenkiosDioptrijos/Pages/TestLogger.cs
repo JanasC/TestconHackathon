c08dc90c90666c77fd0cacbee2d89c5769510681
Martynas Luneckas <Martynas.Luneckas@devbridge.com>
2017-10-20 15:06:22 +0300

    Adding logging output to console

--- PenkiosDioptrijos/Pages/TestLogger.cs
+++ PenkiosDioptrijos/Pages/TestLogger.cs
@@ -13,9 +13,9 @@ namespace Test
         public static void LogMessage(string Message)
         {
             String TimeStamp = DateTime.Now.ToString("u");
-            TestContext.Write(TimeStamp + ": - " + Message+ "\r\n");
-
-
+            String Messsage = (TimeStamp + ": - " + Message + "\r\n");
+            TestContext.Write(Messsage);
+            Console.WriteLine(Messsage);
         }
 
     }
