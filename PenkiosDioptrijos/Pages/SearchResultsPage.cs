8a2634612358e3c89cefe2819166b41f7cdc801a
JanasC <janaschatkevicius@Janass-MacBook-Pro.local>
2017-10-20 12:07:10 +0300

    Playstation4Selector

--- PenkiosDioptrijos/Pages/SearchResultsPage.cs
+++ PenkiosDioptrijos/Pages/SearchResultsPage.cs
@@ -8,8 +8,14 @@ namespace Test
 {
     public class SearchResultsPage
     {
+        private IWebDriver Driver;
+        public SearchResultsPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
         public By SearchBarSelector = By.CssSelector("input.twotabsearchtextbox");
-        private IWebDriver driver;
+        public By Playstation4ConsoleSelector = By.LinkText("a-link-normal s-ref-text-link");
 
     }
 }
