766fd1f3cb24c27e4a3f7305df56029b16e0cea2
Martynas Luneckas <Martynas.Luneckas@devbridge.com>
2017-10-20 14:40:00 +0300

    Logging for ClickElementHelper

--- PenkiosDioptrijos/Helpers/ClickElementHelper.cs
+++ PenkiosDioptrijos/Helpers/ClickElementHelper.cs
@@ -1,17 +1,22 @@
 ﻿using OpenQA.Selenium;
+using Test;
 
 namespace PenkiosDioptrijos.Helpers
 {
     public class ClickElementHelper
     {
         private IWebDriver Driver;
+        private TestLogger _testLogger;
         public ClickElementHelper(IWebDriver driver)
+
         {
             Driver = driver;
         }
 
         public void ClickElement(By by)
         {
+            _testLogger = new TestLogger();
+            _testLogger.LogMessage("Clicking Element: "+ by);
             Driver.FindElement(by).Click();
         }
     }
