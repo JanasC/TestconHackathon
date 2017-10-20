e97109c1199ef195bea6c04d816cc77205e0cae5
Martynas Luneckas <Martynas.Luneckas@devbridge.com>
2017-10-20 13:36:44 +0300

    Logger initial commit

---
+++ PenkiosDioptrijos/Pages/TestLogger.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using PenkiosDioptrijos;
+using OpenQA.Selenium;
+using System.Threading;
+using System;
+using OpenQA.Selenium.Support.UI;
+using NUnit;
+
+namespace Test
+{
+    public class TestLogger
+    {
+        public TestLogger(string driver)
+        {
+            //String TimeStamp = ToString.DateTime.Now();
+            //TestContext.Write()
+
+
+        }
+
+    }
+}
